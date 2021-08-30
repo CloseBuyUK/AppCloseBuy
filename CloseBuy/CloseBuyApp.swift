28dc1756caf73ca66c184f35d560022bfa3ceee6
CloseBuyUK <86831782+CloseBuyUK@users.noreply.github.com>
2021-08-30 21:27:52 +0100

    Initial Commit

---
+++ CloseBuy/CloseBuyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CloseBuyApp.swift
+//  CloseBuy
+//
+//  Created by Connor A Lynch on 30/08/2021.
+//
+
+import SwiftUI
+
+@main
+struct CloseBuyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
